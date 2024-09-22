8c67858fd46d2c802f19ee5cdff490390a3914fc
Philipp Dormann <philipp@philippdormann.de>
2024-09-22 12:52:05 +0200

    chore: update sources

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 import got from 'got';
 import fs from 'fs';
 
-got.get('https://gist.githubusercontent.com/adamawolf/3048717/raw/1ee7e1a93dff9416f6ff34dd36b0ffbad9b956e9/Apple_mobile_device_types.txt').text().then((res) => {
+got.get('https://gist.githubusercontent.com/adamawolf/3048717/raw/bd838e3254565a8da730873667e5d39902fffe6e/Apple_mobile_device_types.txt').text().then((res) => {
     res = res.replaceAll("\n\n", "\n");
     const devices = res.split("\n");
     const array = [];
